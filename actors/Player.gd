bbdab4a1cc741d321a75a1ce4a6344bd6722a3f0
EliasVincent <riesyeti@outlook.de>
2022-04-16 02:02:20 +0200

    color state on player

--- actors/Player.gd
+++ actors/Player.gd
@@ -2,15 +2,22 @@ extends KinematicBody2D
 
 export (int) var speed = 200;
 export (int) var start_hp : int = 3;
-onready var hp := start_hp;
+onready var hp = start_hp;
 var can_take_damage = true;
-onready var animation_player := $AnimationPlayer
+onready var animation_player = $AnimationPlayer
+onready var player_sprite = $Sprite
 
 export (bool) var clamp_to_window_borders = true;
 onready var screen_borders = Vector2(
 	ProjectSettings.get_setting("display/window/size/width"),
 	ProjectSettings.get_setting("display/window/size/height")
 );
+
+enum ColorState {
+	BLACK,
+	WHITE
+}
+onready var color_state = ColorState.BLACK;
 
 func _physics_process(delta):
 	# Input
@@ -23,6 +30,15 @@ func _physics_process(delta):
 	# Clamp to screen borders, make sure we're in the window
 	if clamp_to_window_borders:
 		global_position = Vector2(clamp(global_position.x, 0, screen_borders.x), clamp(global_position.y, 0, screen_borders.y));
+	
+	# ColorState
+	if Input.is_action_just_pressed("change_color"):
+		if color_state == ColorState.BLACK:
+			color_state = ColorState.WHITE;
+			animation_player.play("WHITE");
+		else:
+			color_state = ColorState.BLACK;
+			animation_player.play("BLACK");
 
 # Andere Scripts (wie Bullets) können diese Methode hier aufrufen
 # Player ist so lange unverwundbar wie die Animation dauert
@@ -42,6 +58,14 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 			#Game.change_scene("res://ui/GameOver.tscn")
 			print("YOU DIED")
 
+func get_color_state():
+	return color_state;
 
 func _ready():
+	if color_state == ColorState.BLACK:
+		color_state = ColorState.WHITE;
+		animation_player.play("WHITE");
+	else:
+		color_state = ColorState.BLACK;
+		animation_player.play("BLACK");
 	pass
